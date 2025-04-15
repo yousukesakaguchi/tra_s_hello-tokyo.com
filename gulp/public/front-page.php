fa3b0faaf8f8d18fc24d44945da21ad78c00f9d0
坂口洋介 <sakaguchi@fredman.jp>
2025-04-16 01:43:21 +0900

    benefit

--- gulp/public/front-page.php
+++ gulp/public/front-page.php
@@ -14,7 +14,36 @@ get_header()
       </div>
     </div>
   </section>
-  <section id="intro"></section><?php
+  <section id="intro">
+    <div class="intro-inner">
+      <div class="intro_title"><span class="en barlow">HELLO TOKYO</span></div>
+      <div class="lead-area">
+        <p class="lead">ハロー、<br />想像以上</p>
+        <div class="intro_text">
+          <p class="text">タクシー業界最大手の日本交通グループに属する株式会社ハロートーキョーは、2003年の創業以来ずっとラグジュアリー路線を走り続けてきました。</p>
+          <p class="text">近年は、GO株式会社とコラボした『Go Reserve』のフラッグシップ企業となり、想像以上の成長と進化、そして想像以上の「働き方」を実現しています。</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="benefit" id="benefit-1">
+    <div class="benefit_inner"><span class="barlow benefit_label">BENEFIT 01</span>
+      <h3 class="benefit_title"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/home/benefit-1-title-1.svg" alt="業界No.1日本交通グループ" /></h3>
+      <figure class="benefit_img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/home/benefit-1-fig-1.jpg" /></figure>
+      <p class="benefit_lead"><span class="gold">“桜にN”</span>を掲げ<br class="m-sm" />
+        <spna class="gold">高品質</spna>を約束します
+      </p>
+      <p class="benefit_body">高品質なサービスを提供する日本交通の“桜にN”を掲げ、タクシーでありながらハイヤーと同等のサービスを提供し続けています。</p>
+    </div>
+  </section>
+  <section class="benefit" id="benefit-2">
+    <div class="benefit_inner"><span class="barlow benefit_label">BENEFIT 02</span>
+      <h3 class="benefit_title"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/home/benefit-2-title-1.svg" alt="アルファード保有数日本交通グループ内No.1" /></h3>
+      <figure class="benefit_img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/home/benefit-2-fig-1.jpg" /></figure>
+      <p class="benefit_lead">移動にほんの少しの<span class="gold">贅沢感</span>を</p>
+      <p class="benefit_body">100%アプリ配車による上質な送迎サービスを提供。おもてなしとは何かを追求し続け「質で日本一」を目指します！</p><a class="m-btn-1 __black benefit_btn" href="<?php echo esc_url( home_url( "/" ) ); ?>about/"><span class="m-btn-1__arrow"><span></span></span><span class="m-btn-1__text">ハロートーキョーについて</span></a>
+    </div>
+  </section><?php
   	$paged = (int) get_query_var('paged');
   	$args = array(
   		'post_type' => 'post',
